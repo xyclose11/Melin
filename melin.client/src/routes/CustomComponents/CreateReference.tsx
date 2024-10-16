4b08a53c91770986dcad4361cfbeab9245f17051
Harris Gustafson <harrisgus1@hotmail.com>
2024-10-15 21:16:27 -0500

    DragNDrop base components

--- melin.client/src/routes/CustomComponents/CreateReference.tsx
+++ melin.client/src/routes/CustomComponents/CreateReference.tsx
@@ -3,19 +3,37 @@
 import { BaseReferenceCreator } from "@/routes/CustomComponents/CreateRefComponents/BaseReferenceCreator.tsx";
 import { ReferenceTypeSelector } from "@/routes/CustomComponents/CreateRefComponents/ReferenceTypeSelector.tsx";
 import { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "@/routes/CustomComponents/DragNDrop/Draggable.tsx";
+import { Droppable } from "@/routes/CustomComponents/DragNDrop/Droppable.tsx";
 export function CreateReference() {
     const [refType, setRefType] = useState("book"); // default is book
-
-    function handleState (newRefType: string) {
+    const [isDropped, setIsDropped] = useState(false);
+    const draggableMarkup = <Draggable>Drag me</Draggable>;
+    function handleState(newRefType: string) {
         setRefType(newRefType);
-        console.log(refType)
+        console.log(refType);
     }
     return (
         <>
             <div className={"grid grid-cols-2 gap-8"}>
-                <ReferenceTypeSelector refType={refType} handleState={handleState} />
-                <BaseReferenceCreator />
+                <DndContext onDragEnd={handleDragEnd}>
+                    {!isDropped ? draggableMarkup : null}
+                    <Droppable>
+                        {isDropped ? draggableMarkup : "Drop here"}
+                    </Droppable>
+                    <ReferenceTypeSelector
+                        refType={refType}
+                        handleState={handleState}
+                    />
+                    <BaseReferenceCreator />
+                </DndContext>
             </div>
         </>
     );
+    function handleDragEnd(event: any) {
+        if (event.over && event.over.id === "droppable") {
+            setIsDropped(true);
+        }
+    }
 }
