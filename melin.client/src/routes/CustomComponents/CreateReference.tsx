6f47d595d02c2a80a7c9edcda8e23e503d6e2157
Harris Gustafson <harrisgus1@hotmail.com>
2024-10-20 15:41:53 -0500

    basic UI for reference creation

--- melin.client/src/routes/CustomComponents/CreateReference.tsx
+++ melin.client/src/routes/CustomComponents/CreateReference.tsx
@@ -3,7 +3,19 @@
 import { ReferenceTypeSelector } from "@/routes/CustomComponents/CreateRefComponents/ReferenceTypeSelector.tsx";
 import { useState } from "react";
 import { BaseReferenceCreator } from "@/routes/CustomComponents/CreateRefComponents/BaseReferenceCreator.tsx";
-import { bookSchema } from "@/routes/ReferenceCreationPages/BaseReferenceSchema.ts";
+import {
+    artWorkSchema,
+    bookSchema,
+    legalCaseSchema,
+    legislationSchema,
+    patentSchema,
+    presentationSchema,
+    primarySourceSchema,
+    recordingSchema,
+    reportSchema,
+    softwareSchema,
+    websiteSchema,
+} from "@/routes/ReferenceCreationPages/BaseReferenceSchema.ts";
 export function CreateReference() {
     const [refType, setRefType] = useState("artwork"); // default is book
 
@@ -21,6 +33,36 @@ export function CreateReference() {
                 />
                 {refType === "book" && (
                     <BaseReferenceCreator refSchema={bookSchema} />
+                )}
+                {refType === "artwork" && (
+                    <BaseReferenceCreator refSchema={artWorkSchema} />
+                )}
+                {refType === "case" && (
+                    <BaseReferenceCreator refSchema={legalCaseSchema} />
+                )}
+                {refType === "patent" && (
+                    <BaseReferenceCreator refSchema={patentSchema} />
+                )}
+                {refType === "legislation" && (
+                    <BaseReferenceCreator refSchema={legislationSchema} />
+                )}
+                {refType === "website" && (
+                    <BaseReferenceCreator refSchema={websiteSchema} />
+                )}
+                {refType === "report" && (
+                    <BaseReferenceCreator refSchema={reportSchema} />
+                )}
+                {refType === "presentation" && (
+                    <BaseReferenceCreator refSchema={presentationSchema} />
+                )}
+                {refType === "primary-source" && (
+                    <BaseReferenceCreator refSchema={primarySourceSchema} />
+                )}
+                {refType === "audio-recording" && (
+                    <BaseReferenceCreator refSchema={recordingSchema} />
+                )}
+                {refType === "software" && (
+                    <BaseReferenceCreator refSchema={softwareSchema} />
                 )}
             </div>
         </>
