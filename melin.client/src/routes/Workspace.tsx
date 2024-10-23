27d52b8357507750004680e7dc866fd6d77f4b25
xyclose11 <harrisgus1@hotmail.com>
2024-10-23 15:13:53 -0500

    page correctly extends

--- melin.client/src/routes/Workspace.tsx
+++ melin.client/src/routes/Workspace.tsx
@@ -1,28 +1,9 @@
-﻿import { WorkspaceToolBar } from "@/routes/CustomComponents/WorkspaceToolBar.tsx";
-import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { Outlet } from "react-router-dom";
+﻿import { Outlet } from "react-router-dom";
 
 export function Workspace() {
     return (
-        <ResizablePanelGroup direction="horizontal">
-            <ResizablePanel
-                collapsible={true}
-                defaultSize={5}
-                maxSize={7}
-                collapsedSize={3}
-            >
-                <WorkspaceToolBar />
-            </ResizablePanel>
-            <ResizableHandle withHandle className="w-1 m-1" />
-            <ResizablePanel id="WorkspaceContent">
-                <div className="flex p-4 justify-center flex-wrap">
-                    <Outlet />
-                </div>
-            </ResizablePanel>
-        </ResizablePanelGroup>
+            <div className="flex p-4 justify-center flex-wrap">
+                <Outlet />
+            </div>
     );
 }
--- melin.client/src/routes/root.tsx
+++ melin.client/src/routes/root.tsx
@@ -1,17 +1,36 @@
 import { NavBar } from "@/routes/Layout.tsx";
 import { Workspace } from "@/routes/Workspace.tsx";
+import { WorkspaceToolBar } from "@/routes/CustomComponents/WorkspaceToolBar.tsx";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Root() {
     return (
-        <>
+        <div className={"mt-16 min-h-dvh"}>
             <NavBar />
-            <div
-                className={
-                    "flex mt-16 justify-center items-center w-screen overflow-auto"
-                }
-            >
-                <Workspace />
-            </div>
-        </>
+            <ResizablePanelGroup  direction="horizontal">
+                <ResizablePanel
+                    collapsible={true}
+                    defaultSize={5}
+                    maxSize={7}
+                    collapsedSize={3}
+                >
+                    <WorkspaceToolBar />
+                </ResizablePanel>
+                <ResizableHandle withHandle className="w-1 m-1" />
+                <div
+                    className={
+                        "flex justify-center items-center w-screen"
+                    }
+                >
+                    <ResizablePanel id="WorkspaceContent">
+                        <Workspace />
+                    </ResizablePanel>
+                </div>
+            </ResizablePanelGroup>
+        </div>
     );
 }
