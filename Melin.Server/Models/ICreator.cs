99cdddb6ff097e2411a939ccdf7ba7e19946eaab
Harris Gustafson <harris.gustafson@valpo.edu>
2024-10-23 17:29:04 -0500

    Backend Creator enum

--- Melin.Server/Models/ICreator.cs
+++ Melin.Server/Models/ICreator.cs
@@ -2,9 +2,33 @@
 
 namespace Melin.Server.Models;
 
+public enum CreatorTypes {
+    Author,
+    Editor,
+    Series_Editor,
+    Translator,
+    Reviewed_Author,
+    Artist,
+    Performer,
+    Composer,
+    Director,
+    Podcaster,
+    Cartographer,
+    Programmer,
+    Presenter,
+    Interview_With,
+    Interviewer,
+    Recipient,
+    Sponsor,
+    Inventor
+}
+
 public class Creator
 {
     public int Id { get; set; }
+
+    public CreatorTypes Types { get; set; } = CreatorTypes.Author;
+
     [MaxLength(512)]
     public string FirstName { get; set; } = "";
     
