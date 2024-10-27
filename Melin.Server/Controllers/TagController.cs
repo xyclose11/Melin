f349faa85af4b46a31c71a86c9bc4891c804be0b
Harris Gustafson <harrisgus1@hotmail.com>
2024-10-27 13:03:37 -0500

    Tag basic CRUD

--- Melin.Server/Controllers/TagController.cs
+++ Melin.Server/Controllers/TagController.cs
@@ -3,40 +3,135 @@ using Melin.Server.Models.Context;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Melin.Server.Controllers;
 
 public class TagController : ControllerBase
 {
-    private readonly TagContext _tagContext;
+    private readonly ReferenceContext _referenceContext;
     private readonly UserManager<IdentityUser> _userManager;
 
-    public TagController(TagContext tagContext, UserManager<IdentityUser> userManager)
+    public TagController(ReferenceContext referenceContext, UserManager<IdentityUser> userManager)
     {
-        _tagContext = tagContext;
+        _referenceContext = referenceContext;
         _userManager = userManager;
     }
+    
+    // GET: Single Tag
+    [HttpGet("get-tag")]
+    [Authorize]
+    public async Task<ActionResult<Tag>> GetSpecificTag(string tagName)
+    {
+        try
+        {
+            var t = await _referenceContext.Tags
+                .Where(t => t.Text == tagName)
+                .FirstAsync();
+
+            if (t != null)
+            {
+                return Ok(t);
+            }
+            
+            return NotFound();
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
+        
+    // GET: All owned tags
+    [HttpGet("get-owned-tags")]
+    [Authorize]
+    public async Task<ActionResult<Tag>> GetOwnedTags()
+    {
+        try
+        {
+            var curUser = User.Identity.Name;
+            if (curUser == null)
+            {
+                return NotFound("User not logged in or User not found");
+            }
+            
+            var t = await _referenceContext.Tags
+                .Where(t => t.CreatedBy == curUser)
+                .ToListAsync();
+            
+
+            if (t != null)
+            {
+                return Ok(t);
+            }
+
+            return NotFound();
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
     
     // POST: Single Tag creation
     [HttpPost("create-tag")]
     [Authorize]
     public async Task<ActionResult<Tag>> PostTag([FromBody] Tag tag)
     {
-        CheckUserAuth();
-        
-        
 
-        
-        return Ok();
-    }
-    
-    private IActionResult CheckUserAuth()
-    {
-        if (User.Identity.IsAuthenticated)
+        try
         {
+            // check if tag already exists
+            var t = await _referenceContext.Tags.ContainsAsync(tag); // TODO test this for functionality
+            if (t)
+            {
+                return NoContent(); // TODO test this and replace with duplicate thing instead
+            }
+
+            _referenceContext.Tags.Add(tag);
+
+            await _referenceContext.SaveChangesAsync();
+            
             return Ok();
         }
-
-        return Unauthorized("User is not authenticated.");
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
     }
+    
+    // DELETE: Delete single Tag
+    [HttpPost("delete-tag")]
+    [Authorize]
+    public async Task<ActionResult<bool>> DeleteTag(int tagId)
+    {
+        try
+        {
+            Tag t = await _referenceContext.Tags.FindAsync(tagId);
+            if (t == null)
+            {
+                return NotFound();
+            }
+            
+            _referenceContext.Tags.Remove(t);
+            
+            await _referenceContext.SaveChangesAsync();
+            
+            return Ok(true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
 }
