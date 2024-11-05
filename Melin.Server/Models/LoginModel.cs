addeec625da941f06d80a501b68d9ee4a5b183de
Harris Gustafson <harris.gustafson@valpo.edu>
2024-11-05 11:49:46 -0600

    added constructors to loginmodel

--- Melin.Server/Models/LoginModel.cs
+++ Melin.Server/Models/LoginModel.cs
@@ -4,4 +4,13 @@ public class LoginModel
 {
     public string Email { get; set; }
     public string Password { get; set; }
+
+    public LoginModel(string email, string password) {
+        Email = email;
+        Password = password;
+    }
+
+    public LoginModel() {
+
+    }
 }
