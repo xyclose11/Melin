c30e2785fa2c5afe07ff40610845ba3ba3300904
Harris Gustafson <harrisgus1@hotmail.com>
2024-11-19 21:42:43 -0600

    updated tests

---
+++ melin.client/src/tests/components/references/ReferenceTableDisplay.text.tsx
@@ -0,0 +1,10 @@
+﻿import { test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Library } from "@/routes/Library.tsx";
+import userEvent from "@testing-library/user-event";
+
+test("loads and displays references", async () => {
+    render(<Library />);
+
+    await userEvent.click(screen.getByText("Date Published"));
+});
