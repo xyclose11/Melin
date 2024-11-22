98a788041625dcea4bceedad410dd33140822a0d
Harris Gustafson <harrisgus1@hotmail.com>
2024-11-21 22:06:49 -0600

    model validation + unit tests

--- Melin.Server.Tests/ReferenceModelTest.cs
+++ Melin.Server.Tests/ReferenceModelTest.cs
@@ -25,18 +25,32 @@ public class ReferenceModelTest
                     "and allow special cases to occur. This is an example of a title that is too long and should not be allowed to be set in the" +
                     "applicatio. The limit should be around 256 characters at most, although there is a change that" +
                     "there is a reference with a longer title. If this is the case we can take a more thorough look" +
-                    "and allow special cases to occur. This is an example of a body of words t"
+                    "and allow special cases to occur. This is an example of a body of words"
+        };
+        
+        var validationResults = new List<ValidationResult>();
+        var context = new ValidationContext(reference);
+        bool isValid = Validator.TryValidateObject(reference, context, validationResults, true);
+        
+        Assert.Single(validationResults);
+        Assert.False(isValid, "Validation should fail due to exceeding MaxLength");
+    }
+    
+    [Fact]
+    public void Create_Reference_Email_Validation_Returns_Validation_Error()
+    {
+        var reference = new Reference
+        {
+            Title = "Test",
+            OwnerEmail = "thisIsAnInvalidEmail;[]"
         };
 
-        var validation = new List<ValidationResult>();
-        var context = new ValidationContext(reference, null, null);
-        bool isValid = Validator.TryValidateObject(reference, context, validation, true);
-        
-        Assert.False(isValid, "Validation should fail due to exceeding MaxLength");
-        // Assert.Contains(validation,
-        //     validation =>
-        //         validation.ErrorMessage.Contains(
-        //             "The field Title must be a string or array type with a max length of '256' characters."));
+        var results = new List<ValidationResult>();
+        var validationContext = new ValidationContext(reference);
+        bool isValid = Validator.TryValidateObject(reference, validationContext, results, true);
+
+        Assert.Single(results);
+        Assert.False(isValid);
     }
 
     [Fact]
@@ -44,6 +58,12 @@ public class ReferenceModelTest
     {
         var reference = new Reference();
 
-        Assert.NotNull(reference.Id);
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(reference);
+        var isValid = Validator.TryValidateObject(reference, validationContext, validationResults, true);
+
+        Assert.NotNull(reference);
+        Assert.True(isValid);
+        Assert.IsType<int>(reference.Id);
     }
 }
