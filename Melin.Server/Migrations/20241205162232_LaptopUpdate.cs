bf8477ef25ad339e21c705cbf5ec8edf6fadeb66
xyclose11 <harrisgus1@hotmail.com>
2024-12-05 10:57:37 -0600

    laptop update

---
+++ Melin.Server/Migrations/20241205162232_LaptopUpdate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Melin.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class LaptopUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Reference",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Reference",
+                type: "integer",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
