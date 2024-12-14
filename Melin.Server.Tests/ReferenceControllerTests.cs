988b7cd2d9750f55492775d7cfc7a67d9cd5aa8c
Harris Gustafson <harrisgus1@hotmail.com>
2024-12-14 15:53:27 -0600

    ReferenceController integration tests

--- Melin.Server.Tests/ReferenceControllerTests.cs
+++ Melin.Server.Tests/ReferenceControllerTests.cs
@@ -1,21 +1,28 @@
 using System.Net;
 using System.Net.Http.Headers;
+using Melin.Server.Models;
 using Melin.Server.Models.References;
+using Melin.Server.Models.Repository;
+using Melin.Server.Services;
 using Melin.Server.Tests.Auth;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Melin.Server.Tests;
 
+// TODO CREATE A TEST DATABASE TO TEST API ENDPOINTS WITH ACTUAL USER DATA
 /// <summary>
 /// Heavily inspired by Microsoft's Official ASP.NET Core 8.0 Documentation on
 /// Integration Testing, which can be found here: https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#basic-tests-with-the-default-webapplicationfactory
 /// </summary>
 public class ReferenceControllerTests : IClassFixture<WebApplicationFactory<Program>> {
     private readonly WebApplicationFactory<Program> _factory;
-
+    private readonly ReferenceService _referenceService;
     public ReferenceControllerTests(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
@@ -26,8 +33,79 @@ public class ReferenceControllerTests : IClassFixture<WebApplicationFactory<Prog
             Id = 0,
             Title = "TEST_TITLE",
             DatePublished = DateTime.UtcNow,
-            OwnerEmail = "MelinTestUser@test.com"
+            OwnerEmail = "MelinTestUser@test.com",
+            Creators = new List<Creator>
+            {
+                new()
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    ReferenceId = 0,
+                    Types = CreatorTypes.Author
+                },
+                new()
+                {
+                    Id = 2,
+                    FirstName = "Mary",
+                    LastName = "Lee",
+                    ReferenceId = 0,
+                    Types = CreatorTypes.Editor
+                }
+            },
+            Language = Language.English,
+            PageAmount = 312,
+            Place = "New York, USA",
+            Tags = new List<Tag>
+            {
+                new()
+                {
+                    Id = 3,
+                    Text = "NYC",
+                    Description = "Novels Written in New York City",
+                    CreatedBy = "MelinTestUser@test.com"
+                },
+                new()
+                {
+                    Id = 4,
+                    Text = "Vintage",
+                    Description = "Novels 75+ years old",
+                    CreatedBy = "MelinTestUser@test.com"
+                },
+                new()
+                {
+                    Id = 5,
+                    Text = "Action",
+                    Description = "",
+                    CreatedBy = "MelinTestUser@test.com"
+                },
+                new()
+                {
+                    Id = 6,
+                    Text = "Historic",
+                    Description = "Historic Novels",
+                    CreatedBy = "MelinTestUser@test.com"
+                }
+            }
         };
+        
+        // in-memory database
+        var options = new DbContextOptionsBuilder<ReferenceContext>()
+            .UseInMemoryDatabase(databaseName: "MelinTestDatabase")
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        var context = new ReferenceContext(options);
+
+        context.Reference.Add(reference1);
+        
+        context.SaveChanges();
+
+        var cache = new MemoryCache(new MemoryCacheOptions());
+
+        var repository = new ReferenceRepository(context, cache);
+
+        _referenceService = new ReferenceService(repository, cache);
     }
 
     [Fact]
@@ -74,7 +152,7 @@ public class ReferenceControllerTests : IClassFixture<WebApplicationFactory<Prog
             new AuthenticationHeaderValue(scheme: "TestScheme");
         
         // Act
-        var response = await client.GetAsync("/Reference/get-single-reference");
+        var response = await client.GetAsync("/Reference/get-single-reference?refId=0");
         
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
