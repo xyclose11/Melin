a4335fe36e2c2d80ef2771f19d0fe36da1399890
Harris Gustafson <harrisgus1@hotmail.com>
2024-12-24 19:38:50 -0600

    Custom User Model

--- Melin.Server/Models/User/ApplicationUser.cs
+++ Melin.Server/Models/User/ApplicationUser.cs
@@ -1,7 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
-namespace Melin.Server.Models;
+namespace Melin.Server.Models.User;
 
 public class ApplicationUser : IdentityUser
 {
+    /// <summary>
+    /// Gets or Sets the Teams List
+    /// </summary>
+    [Range(0, 16)]
+    public List<Team>? Teams { get; set; }
+    
+    public bool IsActive { get; set; }
+    
+    public DateTime? LastLoginDate { get; set; }
+    
+    public string? SignalRConnectionId { get; set; }
+    
+    public string? DisplayName { get; set; }
 }
