b3b9a9f7da9eb3d9351dbbd84edf9ce3bb04dcdc
xyclose11 <harrisgus1@hotmail.com>
2025-01-02 16:43:55 -0600

    CSLJSON schema progress

--- melin.client/src/utils/CSLJSON.ts
+++ melin.client/src/utils/CSLJSON.ts
@@ -1,14 +1,33 @@
-interface Author {
+interface IContributor {
     given: string;
     family: string;
+    "dropping-particle": string;
+    "non-dropping-particle": string;
+    suffix: string;
+    "comma-suffix": string | number | boolean;
+    "static-ordering": string | number | boolean;
+    literal: string;
+    "parse-names": string | number | boolean;
+    additionalProperties: boolean;
 }
-
+// LEFT OFF ON CURATOR
 export interface CSLJSON {
     id: string;
     type: string;
-    author?: Author[];
+    author?: IContributor[];
+    chair?: IContributor[];
+    "collection-editor"?: IContributor[];
+    compiler?: IContributor[];
+    composer?: IContributor[];
+    "container-author"?: IContributor[];
+    contributor?: IContributor[];
+    curator?: IContributor[];
     title?: string;
     URL?: string;
     keyword?: string;
     ISBN?: string;
+    categories: string[];
+    language: string;
+    journalAbbreviation: string;
+    shortTitle: string;
 }
